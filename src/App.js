cdb8c787af4036b2934a912bcbd056a4ab36b31b
Ztmartin315 <ztmartin315@gmail.com>
2023-07-12 12:07:09 -0400

    Resume Page Setup

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Carousel } from 'react-bootstrap';
+
 
 function Apps() {
 
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        
-      <div class = "navbar navbar-fixed-top">
-         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-             <a class="navbar-brand" href="#">ZackWorld</a>
-             <div class="collapse navbar-collapse" id="navbarNav">
-             <ul class="navbar-nav">
-                  
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Resume</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Gallery</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Guest List</a>
-                  </li>
-                </ul>
-             </div>
-          </nav>
-         </div>
-
-         <div class="jumbotron jumbotron-fluid">
-       <div class="title">
-          <h1>Hello, World!</h1>
-          <h2>Welcome to ZackWorld!</h2>
-          <a class="btn btn-primary" href="#" role="button">About Zack</a>
-       </div>
-       </div>
-
+        <h1>ERROR 404</h1>
       </header>
 
-<body>
+      <body>
+        <h2>Zackworld is not available right now!</h2>
       </body>
     </div>
   );
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/about.css';
 
 
 function About() {
@@ -7,17 +8,24 @@ function About() {
 
 
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className="About">
+            <header className="About-header">
 
 
 
-                <h1>About Page</h1>
+                <div classname="jumbo">
+                    <div class="jumbotron">
+                        <center class="title" ><h1 class="display-4">About Zack!</h1>
+                            <p class="lead">Learn all about Zack!</p>
+                            <hr class="my-4" />
+                            <h5>Learn about my life, interests, hobbies, and more!</h5>
+                        </center>
+                    </div>
+                </div>
             </header>
-
             <body>
 
-                <h2> About Placeholder</h2>
+                <center><h2> Woah</h2></center>
 
 
             </body>
